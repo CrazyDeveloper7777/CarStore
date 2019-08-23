2f14bf591ef966d1b29d1bf3a9ab0fc8e4cf5b06
CrazyDeveloper7777 <god971@abv.bg>
2019-08-23 18:25:26 +0300

    Include deleted files.

--- Data/CarShop.Data.Models/Vehicles/Bus.cs
+++ Data/CarShop.Data.Models/Vehicles/Bus.cs
@@ -1,11 +1,14 @@
 ﻿using CarShop.Data.Models.Vehicles.Contracts;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CarShop.Data.Models.Vehicles
 {
-    public class Bus : LargerVehicle, IBus
+    public class Bus : Vehicle, IBus
     {
+        [Required]
+        public int SeatsCount { get; set; }
     }
 }
